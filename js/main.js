e6ba7c5212cb936369c28adad1ac19f5d75ea25c
Ethan Sanders <ethanedu5@gmail.com>
2018-12-19 23:53:21 -0600

    added responsive nav

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@ function scrollClick(id){
     .getElementById(id)
     .scrollIntoView({ block: 'start', behavior: 'smooth' });
 }
+function toggleNav() {
+    var x = document.getElementById("nav");
+    if (x.className === "nav_container") {
+      x.className += " nav_container_open";
+    } else {
+      x.className = "nav_container";
+    }
+  }
